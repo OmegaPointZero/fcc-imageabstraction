aa231e4da9f21287fc814b2c3958282502f25d43
System <none>
2017-09-27 16:40:22 +0000

    Final Update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const mongo = require('mongodb').MongoClient;
 const mongoStr = 'mongodb://fccUser:fccPassword@ds149934.mlab.com:49934/fcc-imgabstraction';
 const express = require('express');
@@ -16,7 +17,6 @@ app.get('/api/history', function(req,res){
     var recents = "";
     queries.find().sort({_id:-1}).limit(15).toArray(function(err,docs){
       if(err) throw err;
-      console.log(docs);
       for(var i=0;i<docs.length;i++){
         var q = decodeURIComponent(docs[i]['query']);
         recents += q + "\n";
@@ -25,15 +25,15 @@ app.get('/api/history', function(req,res){
       res.end(recents);
     })
   });
-  
-  
-  
 })
 
-app.get('/api/search/*', function(req,res){
-  var qStr = req.url.slice(12);
+app.get('/api/search/:query', function(req,res){
+  //extract search query & make an object to enter into MongoDB
+  var qStr = encodeURIComponent(req.params.query);
+  var offset = req.query.offset;
   var qObj = {};
   qObj['query'] = qStr;
+  //Insert query into MongoDB
   mongo.connect(mongoStr, function(err,db){
     if(err) throw err;
     var queries = db.collection('queries');
@@ -42,9 +42,41 @@ app.get('/api/search/*', function(req,res){
       db.close();
     });
   });
+  //Query the Google Images API
+  var apiKey = 'AIzaSyBekVEsCm3o73VRtxdWeUIh3hoiur0MOiE';
+  var url = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey + '&cx=001441806295693258836:zo_sci2c42w&searchType=image&q=' + qStr;
+if(offset != undefined){
+  url += '&start=' + offset;
+}
   
-  res.end(qStr);
-})
+  
+  var options = {
+    uri: url,
+    method: 'GET',
+  }
+  
+  request(options, function(err, response, body){
+    if(err) {
+      console.log(err);
+    }
+    var arr = [];
+    var result = JSON.parse(body);
+    
+    console.log(result.items[0]);
+    for(var i=0;i<result.items.length;i++){
+      var picInfo = {
+        'url' : result.items[i]['link'],
+        'alt-text' : result.items[i]['snippet'],
+        'page-url' : result.items[i]['image']['contextLink']
+      }
+      
+      arr.push(picInfo);
+    }
+    
+    res.end(JSON.stringify(arr));
+  });
+  
+});
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
